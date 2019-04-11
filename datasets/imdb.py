9ae00828f8746b3a215bd0e7edcff0653ef87498
Railag <whenthegroundcavedin@gmail.com>
2019-04-11 17:48:42 +0300

    imdb & CIFAR-10 default samples

--- datasets/imdb.py
+++ datasets/imdb.py
@@ -1,11 +1,47 @@
+import keras.layers as layers
+import keras.models as models
+import numpy as np
 from keras.datasets import imdb
 
-(x_train, y_train), (x_test, y_test) = imdb.load_data(path="imdb.npz",
-                                                      num_words=None,
-                                                      skip_top=0,
-                                                      maxlen=None,
-                                                      seed=113,
-                                                      start_char=1,
-                                                      oov_char=2,
-                                                      index_from=3)
-print("imdb dataset loaded")
+(training_data, training_targets), (testing_data, testing_targets) = imdb.load_data(num_words=10000)
+data = np.concatenate((training_data, testing_data), axis=0)
+targets = np.concatenate((training_targets, testing_targets), axis=0)
+
+
+def vectorize(sequences, dimension=10000):
+    results = np.zeros((len(sequences), dimension))
+    for i, sequence in enumerate(sequences):
+        results[i, sequence] = 1
+    return results
+
+
+data = vectorize(data)
+targets = np.array(targets).astype("float32")
+test_x = data[:10000]
+test_y = targets[:10000]
+train_x = data[10000:]
+train_y = targets[10000:]
+model = models.Sequential()
+# Input - Layer
+model.add(layers.Dense(50, activation="relu", input_shape=(10000,)))
+# Hidden - Layers
+model.add(layers.Dropout(0.3, noise_shape=None, seed=None))
+model.add(layers.Dense(50, activation="relu"))
+model.add(layers.Dropout(0.2, noise_shape=None, seed=None))
+model.add(layers.Dense(50, activation="relu"))
+# Output- Layer
+model.add(layers.Dense(1, activation="sigmoid"))
+model.summary()
+# compiling the model
+model.compile(
+    optimizer="adam",
+    loss="binary_crossentropy",
+    metrics=["accuracy"]
+)
+results = model.fit(
+    train_x, train_y,
+    epochs=2,
+    batch_size=500,
+    validation_data=(test_x, test_y)
+)
+print("Test-Accuracy:", np.mean(results.history["val_acc"]))
