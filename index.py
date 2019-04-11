4d1ef2896bc12e761af58c05b22026a366236142
Railag <whenthegroundcavedin@gmail.com>
2019-04-11 15:05:41 +0300

    added time series dataset (average temperature)

--- index.py
+++ index.py
@@ -19,6 +19,8 @@ from NN_types.GRU import predict as GRU_predict
 from NN_types.LSTM import predict as LSTM_predict
 from NN_types.MLP import predict as MLP_predict
 from NN_types.RNN import predict as RNN_predict
+from datasets.average_temperature import training_data as average_temperature_training_data, test_data as \
+    average_temperature_test_data
 from utils import prepare_image
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
@@ -53,14 +55,19 @@ def image_feature_recognition(image_path):
 
 
 print("NN architectures")
-image_feature_recognition('test.jpg')
+# image_feature_recognition('test.jpg')
+image_feature_recognition('madrid.jpg')
 
 print("\n\n")
 print("NN types")
-training_input_1 = [10, 20, 30, 40, 50, 60, 70, 80, 90]
-test_input_1 = [70, 80, 90]
-time_series(training_input_1, test_input_1)
+# training_input_1 = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+# test_input_1 = [70, 80, 90]
+# time_series(training_input_1, test_input_1)
+#
+# training_input_2 = [10, 20, 30, 40, 50, 60, 70, 80, 90, 80, 70, 60, 50, 40]
+# test_input_2 = [90, 80, 100]
+# time_series(training_input_2, test_input_2)
 
-training_input_2 = [10, 20, 30, 40, 50, 60, 70, 80, 90, 80, 70, 60, 50, 40]
-test_input_2 = [90, 80, 100]
-time_series(training_input_2, test_input_2)
+training_input_3 = average_temperature_training_data
+test_input_3 = average_temperature_test_data
+time_series(training_input_3, test_input_3)
